78b0f48e881dd72f254eb22a57a0a60667ee0af8
sijun-yang <yangsijun5528@gmail.com>
2026-03-14 11:16:52 +0900

    [WEEK03] 난이도하_큐_카드2_실버4

--- week3/problem-solving/난이도하_큐_카드2_실버4.py
+++ week3/problem-solving/난이도하_큐_카드2_실버4.py
@@ -2,16 +2,31 @@
 # 링크: https://www.acmicpc.net/problem/2164
 # AI 사용 횟수:
 # AI 사용 방법:
-# 목표 시간:
-# 실제 시간:
+# 목표 시간: 30분
+# 실제 시간: 5분 37초
 
 # 1. 초기 접근
-#
+# 선입선출로 처리하면 됨. 하나 버리도 하나 뒤에 넣는걸 반복하면 됨.
+# deque 사용 필요
 
 # 2. 풀이 전략
-#
+# 그대로 사용
 
 # 3. 막힌 점 / 실수
-#
+# 종료 조건에 대한 고민 없이 접근했음. 쉬운 문제가 상관없었지만, 이런 습관은 버리는게 좋은데...
 
 # 4. 최종 코드
+import sys
+from collections import deque
+
+num = int(sys.stdin.readline().strip())
+
+q = deque(list(range(1, num + 1)))
+cnt = 0
+
+while len(q) > 1:
+    q.popleft()
+    q.append(q.popleft())
+    cnt += 1
+
+print(q.popleft())
