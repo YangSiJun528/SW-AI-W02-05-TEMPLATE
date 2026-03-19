314ce4cc5fc0403470af2bb3c89193c3b7d4479d
sijun-yang <yangsijun5528@gmail.com>
2026-03-19 14:26:23 +0900

    기본 템플릿 변경

--- week4/problem-solving/Extra_트리_ImplementTriePrefixTree.py
+++ week4/problem-solving/Extra_트리_ImplementTriePrefixTree.py
@@ -2,7 +2,7 @@
 # 링크: https://leetcode.com/problems/implement-trie-prefix-tree/?envType=study-plan-v2&envId=top-interview-150
 # AI 사용 횟수:
 # AI 사용 방법:
-# 목표 시간:
+# 목표 시간: 60분
 # 실제 시간:
 
 # 1. 초기 접근
@@ -12,6 +12,7 @@
 #
 
 # 3. 막힌 점 / 실수
+# 유형: [접근법] [구현] [엣지케이스] [최적화]
 #
 
 # 4. 최종 코드
