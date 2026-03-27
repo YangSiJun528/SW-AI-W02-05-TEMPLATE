3732f50fc3f967cb22458e433f4d9f58585fbb99
sijun-yang <yangsijun5528@gmail.com>
2026-03-27 22:39:35 +0900

    [WEEK04] 난이도 하 전체

--- week5/problem-solving/난이도하_그리디_동전0_실버4.py
+++ week5/problem-solving/난이도하_그리디_동전0_실버4.py
@@ -3,9 +3,11 @@
 # AI 사용 횟수:
 # AI 사용 방법:
 # 목표 시간: 15분
-# 실제 시간:
+# 실제 시간: 6분 4초
 
 # 1. 초기 접근
+# 이거는 유명한 유형이라 본 영상에서도 나왔음.
+# 큰 동전부터 계산 가능. 왜냐면 배수인 경우를 문제에서 보장하므로.
 #
 
 # 2. 풀이 전략
@@ -16,3 +18,23 @@
 #
 
 # 4. 최종 코드
+import sys
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+coins = [int(input().strip()) for _ in range(n)]
+
+coins = coins[::-1]
+
+#print(coins)
+
+cnt = 0
+
+for coin in coins:
+    n = k // coin
+    k = k % coin
+    cnt += n
+
+print(cnt)
+
